bea6698cd2972fb7e6acccb86f8c58b3f515c5ed
CameronDavis88 <cammien88@gmail.com>
2021-12-17 15:14:41 -0700

    Put controllers in their own file etc..

---
+++ server/controllers/controllers.js
@@ -0,0 +1,26 @@
+const {books} = require('../../db/bookList')
+
+module.exports = {
+    getBooks : (req, res) => {
+        res.status(200).json(books)
+    },
+    addBook : (req, res) => {
+        const newBook = req.body;
+        books.push(newBook)
+        res.status(200).json(books)
+    },
+    updateBook : (req, res) => {
+        const {id} = req.params
+        const {book} = req.body
+        const index = books.findIndex(book => book.isbn == id)
+        books.splice(index, 1, book )
+        res.status(200).json(books)
+    },
+    deleteBook : (req, res) => {
+        const {id} = req.params
+        const index = books.findIndex(book => book.isbn == id)
+        books.splice(index, 1)
+        res.status(200).json(books)
+    },
+
+}
--- server/index.js
+++ server/index.js
@@ -1,35 +1,18 @@
 const express = require('express');
-const {books} = require('../db/bookList')
+const {getBooks, addBook, updateBook, deleteBook} = require('./controllers/controllers')
 const port = 4444;
 
 const app = express();
 app.use(express.json());
 
 
-app.get(`/api/books`, (req, res) => {
-    res.status(200).json(books)
-})
+app.get(`/api/books`, getBooks)
 
-app.post(`/api/book`, (req, res) => {
-    const newBook = req.body;
-    books.push(newBook)
-    res.status(200).json(books)
-})
+app.post(`/api/book`, addBook)
 
-app.put(`/api/book/:id`, (req, res) => {
-    const {id} = req.params
-    const {book} = req.body
-    const index = books.findIndex(book => book.isbn == id)
-    books.splice(index, 1, book )
-    res.status(200).json(books)
-} )
+app.put(`/api/book/:id`, updateBook )
 
-app.delete(`/api/book/:id`, (req, res) => {
-    const {id} = req.params
-    const index = books.findIndex(book => book.isbn == id)
-    books.splice(index, 1)
-    res.status(200).json(books)
-})
+app.delete(`/api/book/:id`, deleteBook)
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
