ed55ace52b41ec760e42e033c29b89b616d4febf
CameronDavis88 <cammien88@gmail.com>
2022-02-01 09:12:47 -0700

    Added hooks to Authentication component.

--- src/components/Dashboard/Authentication.js
+++ src/components/Dashboard/Authentication.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 //    This component will be rendered instead of the Dashboard when the user clicks login or register 
 //-- and will conditionally render depending on if the user is registering or logging in
 // ---- it will be router through routes 
 
-const Authentication = () => {
+const Authentication = (props) => {
+  const [usernameInput, setUsernameInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [passwordConfirmationInput, setPasswordConfirmationInput] = useState('');
+
+  useEffect(() => {
+    console.log(props)
+  }, [])
+
   return (
     <div>
       This is the Authentication component
+
+
     </div>
   )
 };
