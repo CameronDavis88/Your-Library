7acd89b44bf03003d7581cf82333dae44dd11930
CameronDavis88 <cammien88@gmail.com>
2022-02-01 09:09:33 -0700

    Baby steps to get Authentication component working.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 //codingMuse();
 import React from 'react';
-import {  HashRouter, Routes, Route } from 'react-router-dom';
+// import {  HashRouter, Routes, Route } from 'react-router-dom';
 import Authentication from './components/Dashboard/Authentication';
-import UsersLibrary from './components/UsersLibrary/UsersLibrary';
-import Dashboard from './components/Dashboard/Dashboard';
-import Navbar from './Navbar/Navbar';
+// import UsersLibrary from './components/UsersLibrary/UsersLibrary';
+// import Dashboard from './components/Dashboard/Dashboard';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <HashRouter>
+      <Authentication/>
+      {/* <HashRouter>
         <Routes>
           <Route exact path='/' element={<Dashboard/>} />
           <Route exact path='/authentication' element={<Authentication/>} />
           <Route exact path='/users_library' element={<UsersLibrary/>} />
 
         </Routes>
-      </HashRouter>
+      </HashRouter> */}
       {/* {routes}  --- When routes are put in no need to render Dashboard here, just set it's exact path to '/'    */}
       {/* <Dashboard/> */}
     </div>
--- src/components/Dashboard/Authentication.js
+++ src/components/Dashboard/Authentication.js
@@ -7,7 +7,7 @@ import React from 'react';
 const Authentication = () => {
   return (
     <div>
-      
+      This is the Authentication component
     </div>
   )
 };
