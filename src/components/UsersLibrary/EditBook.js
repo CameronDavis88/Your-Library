98f372d22091525c9b4c076e4d4453cc1d1b46f4
CameronDavis88 <cammien88@gmail.com>
2022-02-03 15:49:18 -0700

    Simple editing of layout of EditBook components.

--- src/components/UsersLibrary/EditBook.js
+++ src/components/UsersLibrary/EditBook.js
@@ -1,23 +1,12 @@
 import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-// import { updateBook } from '../../../server/controllers/mainControllers';
 
-const EditBook = ({ id, title, author, imageUrl, usersBookId, deleteBook, setEditMode, editMode, getBooks }) => {
+const EditBook = ({ id, title, author, imageUrl, deleteBook, setEditMode, getBooks }) => {
    const [newTitle, setNewTitle] = useState('');
    const [newAuthor, setNewAuthor] = useState('');
    const [newImageUrl, setNewImageUrl] = useState('');
-//    const [editMode, setEditMode] = useState(false);
 
-//if this conditional assigning of NewTitle newAuthor and newImageUrl works see if you can just put every thing after the = into the onClick below
-// const handleTitleInput = (e) => setNewTitle(e ? e.target.value : title);
-// const   handleAuthorInput = (e) => setNewAuthor(e ? e.target.value : author);
-// const   handleImageUrlInput = (e) => setNewImageUrl(e ? e.target.value : imageUrl);
-
-//    const  handleTitleInput = (e) => setNewTitle(e.target.value);
-//    const  handleAuthorInput = (e) => setNewAuthor(e.target.value);
-//     const handleImageUrlInput = (e) => setNewImageUrl(e.target.value);
-   
     const editBook = async () => {
         const updatedBook = {
             //If the input is empty the value of the key is the original value
@@ -33,33 +22,34 @@ const EditBook = ({ id, title, author, imageUrl, usersBookId, deleteBook, setEdi
             .catch(err => console.log(err))
     };
 
-    useEffect(() => {
-        console.log(newTitle)
-        console.log(newAuthor)
-    })
+    // useEffect(() => {
+    // })
    
         return (
             <div>
-              
-                    <div>
+                        <h3>Go ahead and customize how your book will appear in your library</h3>
+                        <div>
+                            <h4> Current Title: {title}</h4>
                         <input onChange={(e) => setNewTitle(e.target.value)} placeholder='New Title' value={newTitle} />
+                        </div>
+                       
+                        <div>
+                        <h4>Current Author's name: {author}</h4>
                         <input onChange={(e) => setNewAuthor(e.target.value)} placeholder='Edit Author(s)' value={newAuthor} />
+                        </div>
+                        <br/>
+                       
+                        <div>
+                           
+                        <img alt='cover sample' src={imageUrl} />
+                        <h4>Current Image Url: {imageUrl}</h4>
                         <input onChange={(e) => setNewImageUrl(e.target.value)} placeholder='New Cover Image Url' value={newImageUrl} />
-
-                        {/* If the conditional value assigning does not work in the input handle functions -- try using this below
-                        <input onChange={handleTitleInput} placeholder='Edit Title' value={newTitle === '' ? title : newTitle}/>
-                        <input onChange={handleAuthorInput} placeholder='Edit Author(s)' value={newAuthor === '' ? author : newAuthor}/>
-                        <input onChange={handleImageUrlInput} placeholder='Edit Image Url' value={newImageUrl === '' ? imageUrl: newImageUrl}/> */}
-
-                        {/* <input onChange={handleTitleInput} placeholder={title} value={newTitle === '' ? title : newTitle}/>
-                        <input onChange={handleAuthorInput} placeholder={author} value={newAuthor === '' ? author : newAuthor}/>
-                        <input onChange={handleImageUrlInput} placeholder={imageUrl} value={newImageUrl === '' ? imageUrl: newImageUrl}/> */}
-
+                        </div>
+                        <br/>
                         <button onClick={editBook}>Submit Update</button>
+                        <br/>
                         <button onClick={() => deleteBook()} >Delete Book</button>
                         <button onClick={() => setEditMode(false)}>Cancel/Go back</button>
-                    </div>
-                
             </div>
         )
 };
