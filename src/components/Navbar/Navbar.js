dac96c9f180d575489c690accf1a589b065fd280
CameronDavis88 <cammien88@gmail.com>
2022-02-04 14:25:40 -0700

    Cleaned up Navbar a bit.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,100 +3,69 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 
 
-
-
 const Navbar = ({ props }) => {
   const [loggedIn, setLoggedIn] = useState(false);
 
-
   useEffect(() => {
-
-console.log(props)
-if(props.user.user_id){
-  setLoggedIn(true)
-}
+    console.log(props)
+    if (props.user.user_id) {
+      setLoggedIn(true)
+    }
   }, [])
 
+  const handleLogout = async () => {
+    props.history.push('/');
+    await axios.get(`/api/logout`)
+      .then(() => {
+        props.clearUser();
 
-const handleLogout = async () => {
-  props.history.push('/');
-await axios.get(`/api/logout`)
-.then(() => {
-  props.clearUser();
- 
-})
-.catch(err => console.log(err))
-};
-
-
-
+      })
+      .catch(err => console.log(err))
+  };
 
   return (
     <div>
----------This is the navbar---------
-{/* START of outermost bracket */}
-{ props.location.pathname === '/' ?
-<div>
-{ loggedIn === true ?
-<>
-<button onClick={handleLogout} >Logout</button>
-<button onClick={() => props.history.push('/users_library')} >Your Library</button>
-<button onClick={() => props.history.push('/authentication')} >Your Account</button>
-</>
-:
-<>
-<button onClick={() => props.history.push('/authentication')} >Login/Register</button>
-</>
-}
-</div>
+      ---------This is the navbar---------
+      {/* START of outermost bracket */}
+      {props.location.pathname === '/' ?
+        <div>
+          {loggedIn === true ?
+            <>
+              <button onClick={handleLogout} >Logout</button>
+              <button onClick={() => props.history.push('/users_library')} >Your Library</button>
+              <button onClick={() => props.history.push('/authentication')} >Your Account</button>
+            </>
+            :
+            <>
+              <button onClick={() => props.history.push('/authentication')} >Login/Register</button>
+            </>
+          }
+        </div>
 
-:    
-// Below here is either Authentication of UsersLibrary
-<div>
+        :
+        // Below here is either Authentication of UsersLibrary
+        <div>
 
-{ props.location.pathname === '/authentication' ?
-<div>
-<button onClick={() => props.history.push('/')} >Back to Public Library</button>
-</div>
-// Below here is usersLibrary
-:
-<div>
-<button onClick={handleLogout} >Logout</button>
-<button onClick={() => props.history.push('/')} >Back to Public Library</button>
-</div>
-}
+          {props.location.pathname === '/authentication' ?
+            <div>
+              <button onClick={() => props.history.push('/')} >Back to Public Library</button>
+            </div>
+            // Below here is usersLibrary
+            :
+            <div>
+              <button onClick={handleLogout} >Logout</button>
+              <button onClick={() => props.history.push('/')} >Back to Public Library</button>
+              <button onClick={() => props.history.push('/authentication')} >Your Account</button>
+            </div>
+          }
 
-</div>
-}
-{/* END of outermost bracket */} 
-
-
-
-
-
-
-
-
-
-
-
-
-{/* {!props.user.user_id ? <>
- <button onClick={() => props.history.push('/authentication')} >Login/Register</button>
-<button onClick={() => props.history.push('/')} >Back to Public Library</button>
- </>
- : 
- <>
-  <button onClick={handleLogout} >Logout</button>
- </>
-} */}
-
-
-
+        </div>
+      }
+      {/* END of outermost bracket */}
     </div>
   )
 };
 
 const mapStateToProps = (reduxState) => reduxState;
 
-  export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps)(Navbar);
