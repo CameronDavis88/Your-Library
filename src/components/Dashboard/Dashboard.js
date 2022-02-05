3f8f77cfcd99e6a3b968d17e2a282fff2d345c08
CameronDavis88 <cammien88@gmail.com>
2022-02-05 11:50:04 -0700

    Search function now still loads if only one page, less that 31 books return.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -18,33 +18,158 @@ class Dashboard extends Component {
         };
     };
 
+
+    //    nextBtn = async (pageNum) => {
+    //     const { searchView, titleSearch, authorSearch, page, books, arrOfArrOfBooks, } = this.state;
+    //     if (searchView === true) {
+    //         //---------------- searchView === true-----------------------------
+    //         console.log('-----Switching to searchView pages--------');
+    //         this.setState({ searchView: true });
+    //         await axios.get(`http://gutendex.com/books?page=${pageNum}&search=${authorSearch}+${titleSearch}`)
+    //             .then(({ data }) => {
+
+    //                     this.setState({ page: 1 })
+    //                     axios.get(`http://gutendex.com/books?page=1&search=${authorSearch}+${titleSearch}`)
+    //                         .then(({ data }) => {
+    //                             // this.showMeTheData(data);
+    //                             // this.setState({ books: [...books, data.results] });
+    //                             // this.setState({ books: data.results });
+
+    //                             // let originalBooks = ['start']
+    //                             // originalBooks = data.results
+    //                             // {
+    //                             //     originalBooks.forEach((book, i, arr) => {
+    //                             //         let { authors, formats } = book;
+    //                             //         book.imageUrl = formats["image/jpeg"];
+    //                             //         book.gutUrl = formats["text/html"];
+    //                             //         book.author = authors[0].name;
+    //                             //     })
+    //                             // }
+    //                             // this.setState({ newBook: originalBooks })
+
+    //                             // console.log(originalBooks)
+
+
+
+    //                         })
+
+
+    //                         .catch((err) => console.log(err))
+    //                 } else {
+    //                     this.setState({ page: page + 1 });
+    //                     axios.get(`${data.next}`).then(({ data }) => {
+    //                         // this.showMeTheData(data);
+    //                         // this.setState({ books: data.results });
+    //                         // this.setState({ books: [...books, data.results] });
+    //                         // this.setState({ arrOfArrOfBooks: [...arrOfArrOfBooks, data.results] });
+    //                         // this.setState({ bookArray: data.results });
+    //                         // console.log(data.results)
+
+    //                         // let originalBooks = ['start']
+    //                         // originalBooks = data.results
+    //                         // {
+    //                         //     originalBooks.forEach((book, i, arr) => {
+    //                         //         let { authors, formats } = book;
+    //                         //         book.imageUrl = formats["image/jpeg"];
+    //                         //         book.gutUrl = formats["text/html"];
+    //                         //         book.author = authors[0].name;
+    //                         //     })
+    //                         // }
+    //                         // this.setState({ newBook: originalBooks })
+    //                         // console.log(originalBooks)
+
+    //                         // console.log(data.results)
+
+
+
+    //                     }).catch((err) => console.log(err));
+    //                 }
+    //             })
+    //             .catch((err) => console.log(err));
+
+    //         //-----------default view----------SearchView === false-----------------------
+    //     } else {
+    //         await axios.get(`http://gutendex.com/books?page=${pageNum}`)
+    //             .then(({ data }) => {
+    //                 if (data.next === null) {
+    //                     this.getPubBooks();
+    //                 } else {
+    //                     this.setState({ page: page + 1 });
+    //                     axios.get(`${data.next}`).then(({ data }) => {
+    //                         // this.showMeTheData(data);
+    //                         // this.setState({ books: data.results });
+    //                         // this.setState({ books: [...books, data.results] });
+    //                         // this.testBookFn(books)
+    //                         // this.setState({ books: [...books, data.results] });
+
+    //                         // let originalBooks = ['start']
+    //                         // originalBooks = data.results
+    //                         // {
+    //                         //     originalBooks.forEach((book, i, arr) => {
+    //                         //         let { authors, formats } = book;
+    //                         //         book.imageUrl = formats["image/jpeg"];
+    //                         //         book.gutUrl = formats["text/html"];
+    //                         //         book.author = authors[0].name;
+    //                         //     })
+    //                         // }
+    //                         // this.setState({ newBook: originalBooks })
+
+
+    //                         // console.log(data.results)
+    //                         // console.log(originalBooks)
+
+
+    //                     }).catch((err) => console.log(err));
+    //                 }
+    //             })
+    //             .catch(err => console.log(err));
+    //     };
+
+
+    // };
+
+
+
+
+
+
+
+
+
+
+
     searchFn = async () => {
         this.setState({ searchView: true });
         const { authorSearch, titleSearch } = this.state;
         await axios.get(`http://gutendex.com/books?search=${authorSearch}%20${titleSearch}`)
-        .then( async ({ data }) => {
-            let pageOne = data.results
-            await axios.get(`http://gutendex.com/books?page=2&search=${authorSearch}%20${titleSearch}`)
-        .then( async ({ data }) => {
-            let pageTwo = data.results
-            this.setState({ books: [...pageOne, ...pageTwo] })
-        })
-        .catch(err => console.log(err))
-        })
-        .catch(err => console.log(err));
+            .then(async ({ data }) => {
+                let pageOne = data.results
+                if (data.next === null) {
+                    this.setState({ books: [...pageOne] })
+                } else {
+                    await axios.get(`http://gutendex.com/books?page=2&search=${authorSearch}%20${titleSearch}`)
+                    .then(async ({ data }) => {
+                        let pageTwo = data.results
+                        this.setState({ books: [...pageOne, ...pageTwo] })
+                    })
+                    .catch(err => console.log(err))
+                }
+            })
+            .catch(err => console.log(err));
 
     };
 
-    getPubBooks =  () => {
+
+    getPubBooks = () => {
         axios.get(`http://gutendex.com/books`)
-            .then( async ({ data }) => {
+            .then(async ({ data }) => {
                 let pageOne = data.results
-            await axios.get(`http://gutendex.com/books?page=2`)
-            .then( async ({ data }) => {
-                let pageTwo = data.results
-                this.setState({ books: [...pageOne, ...pageTwo] })
-            })
-            .catch(err => console.log(err))
+                await axios.get(`http://gutendex.com/books?page=2`)
+                    .then(async ({ data }) => {
+                        let pageTwo = data.results
+                        this.setState({ books: [...pageOne, ...pageTwo] })
+                    })
+                    .catch(err => console.log(err))
             })
             .catch(err => console.log(err));
     };
@@ -77,4 +202,8 @@ class Dashboard extends Component {
 const mapStateToProps = (reduxState) => reduxState;
 
 export default connect(mapStateToProps, { clearUser })(Dashboard);
+
+
+
+
 
