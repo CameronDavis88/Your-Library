308247eb1ac689b6cc07b7a049191b29df14ead9
CameronDavis88 <cammien88@gmail.com>
2022-02-05 11:59:08 -0700

    cleaned up App comonent a bit.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 //codingMuse();
 import React from 'react';
-import {  HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Authentication from './components/Dashboard/Authentication';
 import UsersLibrary from './components/UsersLibrary/UsersLibrary';
 import Dashboard from './components/Dashboard/Dashboard';
-import Navbar from './components/Navbar/Navbar';
 const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
-      {/* <Authentication/> */}
       <Router>
         <Switch>
-          {/* <Route exact path='/' element={<Dashboard/>} />
-          <Route path='/authentication' element={<Authentication/>} />
-          <Route  path='/users_library' element={<UsersLibrary/>} /> */}
           <Route exact path='/' component={Dashboard} />
           <Route path='/authentication' component={Authentication} />
-          <Route  path='/users_library' component={UsersLibrary} />
-
+          <Route path='/users_library' component={UsersLibrary} />
         </Switch>
       </Router>
-      {/* {routes}  --- When routes are put in no need to render Dashboard here, just set it's exact path to '/'    */}
-      {/* <Dashboard/> */}
     </div>
-  )
+  );
 };
 
 
 const mapStateToProps = (reduxState) => reduxState;
 
-  export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App);
