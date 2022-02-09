4c92113f9bd8e1c029c21071c4f7edcbc3fbb0d9
CameronDavis88 <cammien88@gmail.com>
2022-02-09 11:49:48 -0700

    Now if the title is too long (over 50 characters) it is truncated to 50 and ends with ...

--- src/components/PubLibrary/PubLibrary.js
+++ src/components/PubLibrary/PubLibrary.js
@@ -11,10 +11,27 @@ const PubLibrary = ({ books }) => {
                 ?
                 <>
                     {books.map((book) => {
-                        const { id, title, authors, formats } = book;
+                        const { id, authors, formats } = book;
+                        let title = book.title;
                         const imageUrl = formats["image/jpeg"];
                         const gutUrl = formats["text/html"];
                         const author = authors[0].name;
+
+                        if (title[51]) {
+                            let newTitle = title.slice(0, 50)
+                            let titleArr = Array.from(newTitle)
+                            titleArr.push('.')
+                            titleArr.push('.')
+                            titleArr.push('.')
+                            let finalTitle = ``
+
+                            titleArr.forEach((ele, ind, arr) => {
+                                finalTitle = finalTitle + `${ele}`
+                                return finalTitle
+                            })
+                            title = finalTitle
+                        }
+
                         return <PubBook key={id} id={id} title={title} author={author}
                             formats={formats} imageUrl={imageUrl} gutUrl={gutUrl} />
                     })}
