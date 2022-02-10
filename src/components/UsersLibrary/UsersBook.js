4f3b35abe0fe63de9b493685fc887d693e33c548
CameronDavis88 <cammien88@gmail.com>
2022-02-10 14:29:00 -0700

    Now asks to confirm before deleting book from Users Library.

--- src/components/UsersLibrary/UsersBook.js
+++ src/components/UsersLibrary/UsersBook.js
@@ -6,11 +6,13 @@ import { Grid, Typography } from '@material-ui/core';
 import './UsersBook.css';
 
 const UsersBook = (props) => {
+    const [deletingView, setDeletingView] = useState(false);
     const { id, author, gutUrl, imageUrl, getBooks, displayTitle, book, routeToSelectedBook } = props;
 
     const deleteBook = () => {
         axios.delete(`/api/book/${id}`)
             .then(({ data }) => {
+                setDeletingView(false)
                 getBooks(data);
             })
             .catch(err => console.log(err));
@@ -24,17 +26,27 @@ const UsersBook = (props) => {
     return (
         <Grid className='book-box'  >
             <div className='book' onClick={goToSelectedBook} >
-            <Typography variant='h6' >{displayTitle}</Typography>
+                <Typography variant='h6' >{displayTitle}</Typography>
                 <Typography >By: {author}</Typography>
-            <div className='image-box' >
-                <img alt='cover' src={imageUrl} className='cover-image-small' />
+                <div className='image-box' >
+                    <img alt='cover' src={imageUrl} className='cover-image-small' />
+                </div>
             </div>
-            </div>
-            <br/>
+            <br />
             <nav>
                 <a className='nav-a' href={gutUrl} >Read Book Here</a>
             </nav>
-            <button className='book-btn' onClick={() => deleteBook()} >Delete Book</button>
+            {deletingView
+                ?
+                <>
+                    <button className='book-btn' onClick={() => deleteBook()}>Confirm Delete</button>
+                    <button className='book-btn' onClick={() => setDeletingView(false)}>Cancel Delete</button>
+
+                </>
+
+                :
+                <button className='book-btn' onClick={() => setDeletingView(true)} >Delete Book</button>
+            }
         </Grid>
     );
 };
