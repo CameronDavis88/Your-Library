6635a9b64255f4b44d3882fb4bb8df3657e8c1ff
CameronDavis88 <cammien88@gmail.com>
2022-02-21 09:55:49 -0700

    For some reason the db file name usersInfo never has been changed on the github side and is wont change from UsersInfo so I just changed the name in the code to UsersInfo.

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -4,20 +4,20 @@ module.exports = {
     register: async (req, res) => {
         const { username, email, password } = req.body;
         const db = req.app.get('db');
-            const [foundUser] = await db.usersInfo.get_user([email]);
+            const [foundUser] = await db.UsersInfo.get_user([email]);
         if (foundUser) {
             return res.status(400).send('Email already in use');
         };
         let salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
-        const [newUser] = await db.usersInfo.add_user([username, hash, email]);
+        const [newUser] = await db.UsersInfo.add_user([username, hash, email]);
                 req.session.user = newUser;
                 res.status(201).send(req.session.user);
 },
     login: async (req, res) => {
         const { email, password } = req.body;
         const db = req.app.get('db');
-        const [foundUser] = await db.usersInfo.get_user([email]);
+        const [foundUser] = await db.UsersInfo.get_user([email]);
         if (!foundUser) {
             return res.status(400).send('Email not found');
         };
@@ -37,11 +37,11 @@ module.exports = {
         const userId = req.params.id;
         const { username, email, password, oldPassword, oldEmail } = req.body;
         const db = req.app.get('db');
-        const [checkEmail] = await db.usersInfo.get_user([email]);
+        const [checkEmail] = await db.UsersInfo.get_user([email]);
         if (checkEmail) {
             return res.status(400).send('Email already in use');
         } else {
-            const [foundUser] = await db.usersInfo.get_user([oldEmail]);
+            const [foundUser] = await db.UsersInfo.get_user([oldEmail]);
             const authenticated = bcrypt.compareSync(oldPassword, foundUser.password);
             if (!authenticated) {
                 return res.status(401).send('Old password is incorrect');
@@ -49,7 +49,7 @@ module.exports = {
         };
         let salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
-        const [updatedUser] = await db.usersInfo.edit_users_info([hash, email, username, userId]);
+        const [updatedUser] = await db.UsersInfo.edit_users_info([hash, email, username, userId]);
         delete updatedUser.password;
         res.status(202).send(updatedUser);
     },
@@ -57,14 +57,14 @@ module.exports = {
         const userId = req.params.id;
         const { email, password } = req.body;
         const db = req.app.get('db');
-        const [foundUser] = await db.usersInfo.get_user([email]);
+        const [foundUser] = await db.UsersInfo.get_user([email]);
         const authenticated = bcrypt.compareSync(password, foundUser.password);
         if (!authenticated) {
             return res.status(401).send('Password is incorrect');
         } else {
             delete foundUser.password;
         req.session.destroy();
-        db.usersInfo.delete_user([userId]);
+        db.UsersInfo.delete_user([userId]);
         res.sendStatus(200);
         };
     }
